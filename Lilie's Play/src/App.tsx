a2d51dc2ee50573f54b50dd7f35aa56912e8fc14
whiteowly <jerryayalew696@gmail.com>
2025-12-08 14:16:29 +0300

    first commit

---
+++ Lilie's Play/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Lilie's Play</h1>
+      <a href="./login.tsx">Login</a><br />
+      <a href="./register.tsx">Register</a>
+
+    
+    </>
+  )
+}
+
+export default App
